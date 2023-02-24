83c0d0bb901eecb1b972a87a2d393a6b083c6dc6
Lukas Mulder <39460488+lukasmulder@users.noreply.github.com>
2023-02-24 12:57:35 +0100

    implemented the algorithm without drift estimation

--- source/goldstandard.py
+++ source/goldstandard.py
@@ -1,24 +1,35 @@
 import numpy as np
 
-points_per_frame = 3
+points_per_frame = 1
 
-num_of_frames = 5
+num_of_frames = 2
 
 
 testinput = np.random.random((num_of_frames, points_per_frame, 2))
-
-print(testinput)
 
 # implement cost function MCF
 
 # implement maximization of cost function w.r.t. drift
 
-# implement algoritmm
 
+# function takes an array of frames each containing an array coordinates
+# there may be drift between the different frames
+# returns a single frame which is the union of the original frames, corrected for drift.
+def gold_standard(input):
     # initialize
+    ref = input[0].tolist() #convert to list because we are going to do a lot of concatenation
 
     # loop:
     # for each new layer maximize the cost function w.r.t. the collection of layers
     # add the current layer to all previous layers with drift correction
 
-    # output the total stack
+    for i in range(1, len(input)):
+        frame = input[i]
+        drift = estimate_drift(frame, ref)
+        # correct the frame by subtracting the drift from all points
+        corrected_frame = frame - np.full(frame.shape, drift)
+        # merge the frames
+        ref += corrected_frame.tolist()
+
+    #output the total stack
+    return np.array(ref)
