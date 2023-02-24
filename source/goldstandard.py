4ce8d38130ddb334bfd28a554472967b35b90d1f
Lukas Mulder <39460488+lukasmulder@users.noreply.github.com>
2023-02-24 12:34:11 +0100

    added general structure of the program

--- source/goldstandard.py
+++ source/goldstandard.py
@@ -1,1 +1,15 @@
 import numpy as np
+
+# implement cost function MCF
+
+# implement maximization of cost function w.r.t. drift
+
+# implement algoritmm
+
+    # initialize
+
+    # loop:
+    # for each new layer maximize the cost function w.r.t. the collection of layers
+    # add the current layer to all previous layers with drift correction
+
+    # output the total stack
